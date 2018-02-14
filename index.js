7281704f4326aeccec1c33f7657c541145feebe8
hvdson <hvdsonmusic@gmail.com>
2018-02-14 06:03:23 +0000

    finished exercise

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let sum = 0;
+
+  array.forEach((item) => {
+    // there can be nested arrays
+
+    // recursive case: if there is an array recursion
+    if (Array.isArray(item)) {
+      // sumItems is going to keep happening until it reaches the inner most item
+      sum += sumItems(item);
+    } else {
+      sum += item;
+    }
+    // base case: have a number return it
+    
+  });
+
+  return sum;
+  
+  
+  
 }
 
 module.exports = sumItems;
